7836bfe1288ba2f6981122021c30e82ca86995c1
Mersed <kahrimanovic.mersed@gmail.com>
2020-02-29 00:07:41 +0100

    Builders - Builder.

---
+++ 6.Builder/2.Builder/cmake-build-debug/CMakeFiles/2_Builder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_Builder.dir/main.cpp.o"
+  "2_Builder.pdb"
+  "2_Builder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_Builder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
