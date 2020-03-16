73afc23b57a7e5cb8e84ed40d0a1ae1f8bc7cf84
Mersed <kahrimanovic.mersed@gmail.com>
2020-03-16 20:42:27 +0100

    Singleton - CodingExercise.

---
+++ 9.Singleton/7.CodingExercise/cmake-build-debug/CMakeFiles/7_CodingExercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/7_CodingExercise.dir/main.cpp.o"
+  "7_CodingExercise.pdb"
+  "7_CodingExercise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_CodingExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
